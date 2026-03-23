1af881db503db6fa204520705d94ce2334096299
Rezvee <50272590+liserezvee@users.noreply.github.com>
2026-03-23 11:09:25 +0100

    steup project

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: string;
+  isVerified?: string;
+}
